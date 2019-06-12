37ddb8b605d3bd5925638350774c51a3e7d29055
Finnbarr O'Callahan <FinnbarrOC@users.noreply.github.com>
2019-06-12 17:04:23 -0400

    Update complete.java

--- day1/complete.java
+++ day1/complete.java
@@ -5,7 +5,6 @@ class Main {
     // A line with two forward slashes is a "comment"; it's greyed-out and does nothing.
     // We'll use them to leave you notes and instructions below!
 
-
     // 1) OUTPUT: first, we want the computer to show us some text
 
     System.out.println("Hello, World!");
@@ -17,13 +16,6 @@ class Main {
     int myNumber = 42;
     System.out.println("My favorite number is " + myNumber);
 
-    // 3) TYPE CONVERSION: all varibles have fixed types, but we can convert between types if needed
-
-    String stringNumber = "10";
-    System.out.println(stringNumber + 7);  // This does NOT give us 17 like we would expect!
-    int intNumber = Integer.parseInt(stringNumber);
-    System.out.println(intNumber + 7);     // This works properly
-
     // 3) INPUT: now let's type a message to the computer, save it, and print it back out
 
     Scanner reader = new Scanner(System.in);
@@ -31,7 +23,7 @@ class Main {
     System.out.println("Type your name, then press Enter:");
     String myName = reader.nextLine();
     System.out.println(myName);
-    System.out.println("My friend " + myName + " is learning to code!");
+    System.out.println("My name is " + myName);
 
     // 4) MATH: try some simple addition/subtraction, then see how fast your computer really is!
 
@@ -39,6 +31,13 @@ class Main {
     System.out.println(12 * 12);
     System.out.println(33432 / 596);
     System.out.println(25 * 24 * 23 / 87);
+
+    // 3) TYPE CONVERSION: all varibles have fixed types, but we can convert between types if needed
+
+    String stringNumber = "10";
+    System.out.println(stringNumber + 7);  // This does NOT give us 17 like we would expect!
+    int intNumber = Integer.parseInt(stringNumber);
+    System.out.println(intNumber + 7);     // This works properly
 
     // 5) IF/ELSE: this is how your computer makes simple decisions based on "True" or "False"
 
@@ -63,6 +62,23 @@ class Main {
       System.out.println("Sorry, wrong answer.");
     }
 
+    // N) MULTIPLE CONDITIONS: this is how we check if multiple things together are true/false
+
+    System.out.println("I'm a polar bear. What color is my fur?");
+    String animalColor = reader.nextLine();
+    System.out.println("How many legs do I have?");
+    int animalLegs = Integer.parseInt(reader.nextLine());
+
+    if (animalColor.equals("white") && animalLegs == 4) {
+      System.out.println("I must be a polar bear!");
+    }
+    else if (animalColor.equals("blue") || animalLegs == 8) {
+      System.out.println("I'm either a fish or a spider?");
+    }
+    else if (!animalColor.equals("white") && animalLegs != 4) {
+      System.out.println("I'm definitely NOT a polar bear then.");
+    }
+
     // 6) CALCULATOR: time to combine everything you've learned to make a simple calculator!
 
     System.out.println("What is the first number?");
@@ -83,11 +99,11 @@ class Main {
 
     while(true) {
       System.out.println("What is the first number?");
-    int numberA = Integer.parseInt(reader.nextLine());
-    System.out.println("What is the second number?");
-    int numberB = Integer.parseInt(reader.nextLine());
-    System.out.println("Would you like to add, subtract, or quit?");
-    String operation2 = reader.nextLine();
+      int numberA = Integer.parseInt(reader.nextLine());
+      System.out.println("What is the second number?");
+      int numberB = Integer.parseInt(reader.nextLine());
+      System.out.println("Would you like to add, subtract, or quit?");
+      String operation2 = reader.nextLine();
 
       if (operation2.equals("quit")) {
         System.out.println("Goodbye");
@@ -106,7 +122,7 @@ class Main {
 
     System.out.println("What number should we find the factorial of?");
     int factorialNumber = Integer.parseInt(reader.nextLine());
-    
+
     int total = 1;
     for (int i = 1; i < factorialNumber + 1; i++) {  // Check for off-by-one errors here
       total = total * i;
