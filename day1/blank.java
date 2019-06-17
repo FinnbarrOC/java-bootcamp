c5c0af06166d5194fb7041be5d4e998a8563b25a
Finnbarr O'Callahan <FinnbarrOC@users.noreply.github.com>
2019-06-17 11:33:43 -0400

    Create blank.java

---
+++ day1/blank.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    // A line with two forward slashes is a "comment"; it's greyed-out and does nothing.
+    // We'll use them to leave you notes and instructions below!
+
+    // 1) OUTPUT: first, we want the computer to show us some text
+
+
+
+    // 2) VARIABLES: we can use variables like labeled boxes to hold data for us
+
+
+
+    // 3) INPUT: now let's type a message to the computer, save it, and print it back out
+
+    Scanner reader = new Scanner(System.in);
+
+
+
+    // 4) MATH: try some simple addition/subtraction, then see how fast your computer really is!
+    //          (because we are using integers, answers will be rounded off to whole numbers)
+    
+
+
+    // 3) TYPE CONVERSION: all varibles have fixed types, but we can convert between types if needed
+
+
+
+    // 6) CALCULATOR: time to combine everything you've learned to make a simple calculator!
+
+
+
+
+    reader.close();
+  }
+}
