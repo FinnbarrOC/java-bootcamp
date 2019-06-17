a816600e14c4d2b19122bee3c57e22cea04ba9f8
Finnbarr O'Callahan <FinnbarrOC@users.noreply.github.com>
2019-06-17 16:46:47 -0400

    Create blank.java

---
+++ day3/blank.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    // 13) ARRAYS: just like a shelf holds boxes, an array holds variables
+
+
+
+    // 14) FUNCTIONS: how we use code that's written somewhere else
+
+    // Write your own function; go to the bottom of the page and fill it in
+    System.out.println(numberTransformer(5));
+
+    // 15) CHARACTERS: single letters or numbers
+
+
+
+    // Strings have built-in functions; let's try some out
+    String helloString = "hello";
+
+
+
+    // 16) ARRAYLISTS: "smart" arrays that have helpful functions built in!
+
+    // Let's first turn an ordinary Array into a shiny new ArrayList
+
+
+    // Now check if a certain letter is in the ArrayList
+
+
+
+    reader.close();
+  }
+
+  public static int numberTransformer(int inputNumber) {
+
+    // Do something here with inputNumber to create outputNumber
+
+
+    return outputNumber;
+  }
+
+}
